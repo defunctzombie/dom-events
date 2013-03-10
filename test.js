64f7deb988fab278fe3d5bae1742cb314ee99e45
Roman Shtylman <shtylman@gmail.com>
2013-03-10 14:55:08 -0400

    use mocha for testing
    tapedeck doesn't work in older browsers which makes it impossible to
    locally debug broken tests

--- test.js
+++ test.js
@@ -1,25 +1,32 @@
-var test = require('tape');
+var assert = require('assert');
 var eve = require('./');
 
-test('on', function(assert) {
-    var div = document.createElement('div');
+// need to append to body for IE testing
+var create = function(what) {
+    var el = document.createElement(what);
+    document.body.appendChild(el);
+    return el;
+};
+
+test('on', function(done) {
+    var div = create('div');
     eve.on(div, 'click', function(ev) {
         assert.ok(ev);
-        assert.end();
-    })
+        done();
+    });
 
     eve.emit(div, 'click');
 });
 
-test('off', function(assert) {
-    var div = document.createElement('div');
+test('off', function(done) {
+    var div = create('div');
 
     var fn = function(ev) {
         assert.ok(ev);
 
         eve.off(div, 'click', fn);
         eve.emit(div, 'click');
-        assert.end();
+        done();
     };
 
     eve.on(div, 'click', fn);
